64dd7ab1508f8bb9e712d80f8efd590eb193b8ce
Yéred <yered.bnet@gmail.com>
2023-03-02 00:17:16 +0100

    construction du processus d ela linked list de philos

--- philo/src/init_philos.c
+++ philo/src/init_philos.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init_philos.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yeye <yeye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/22 11:09:38 by yperonne          #+#    #+#             */
-/*   Updated: 2023/03/01 21:54:08 by yeye             ###   ########.fr       */
+/*   Created: 2023/03/01 21:23:36 by yeye              #+#    #+#             */
+/*   Updated: 2023/03/01 21:53:49 by yeye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-/* Arguments a prendre en compte
 
-Arg 1 = Nombre de philosophes
-Arg 2 = temps avant de mourrir
-Arg 3 = temps de repas
-Arg 4 = temps de repos
-(Arg 5 = Nombre de repas a consommer)*/
-
-int	main(int argc, char **argv)
+void	init_philos_table(int argc, char **argv, t_philo **philos)
 {
-	t_philo **philos;
-	
-	check_args_errors(argc, argv);
-	init_philos_table(argc, argv, philos);
-	return (0);
+
 }
--- philo/src/linked_list_utils.c
+++ philo/src/linked_list_utils.c
@@ -1,29 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   linked_list_utils.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yeye <yeye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/22 11:09:38 by yperonne          #+#    #+#             */
-/*   Updated: 2023/03/01 21:54:08 by yeye             ###   ########.fr       */
+/*   Created: 2023/03/01 21:46:31 by yeye              #+#    #+#             */
+/*   Updated: 2023/03/01 21:54:12 by yeye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-/* Arguments a prendre en compte
 
-Arg 1 = Nombre de philosophes
-Arg 2 = temps avant de mourrir
-Arg 3 = temps de repas
-Arg 4 = temps de repos
-(Arg 5 = Nombre de repas a consommer)*/
-
-int	main(int argc, char **argv)
+t_philo	new_philo(t_philo **philos, int argc, int a)
 {
-	t_philo **philos;
-	
-	check_args_errors(argc, argv);
-	init_philos_table(argc, argv, philos);
-	return (0);
-}
+	t_philo *new_philo;
+	if (!philos)
+	{
+
+	}
+	else
+	{
+
+	}
+}	
